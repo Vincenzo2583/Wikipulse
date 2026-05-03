d6ae27875cf45f470ac8aa238db84ed81e18003c
Vincenzo83 <dayoakindele380@gmail.com>
2026-05-03 20:22:20 +0100

    Remove config.py from repository

--- config.py
+++
@@ -1,4 +0,0 @@
-MONGO_URI = "mongodb://localhost:27017/wikipulse"
-DB_NAME = "wikipulse"
-COLLECTION_NAME = "recent_changes"
-WIKIPEDIA_STREAM_URL = "https://stream.wikimedia.org/v2/stream/recentchange"
